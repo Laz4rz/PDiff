703568ea450f1ce59103434848edef492da2ca99
Laz4rz <boronskimikolaj@gmail.com>
2026-02-02 16:13:21 +0100

    add: tiny support

--- examples/gidd/owt.sh
+++ examples/gidd/owt.sh
@@ -7,14 +7,14 @@ cd "${REPO_ROOT}" || exit 1
 export PYTHONPATH="src:${PYTHONPATH:-}"
 
 python -m src.discrete_diffusion \
-  data=openwebtext \
-  model=small \
+  data=tinystories \
+  model=tiny \
   algo=gidd \
   lr_scheduler=cosine_decay_warmup \
   noise=geometric \
-  trainer.num_nodes=1 trainer.devices=8 \
+  trainer.num_nodes=1 trainer.devices=2 \
   trainer.max_steps=500000 \
-  trainer.accumulate_grad_batches=2 \
+  trainer.accumulate_grad_batches=2 * 4 \
   loader.global_batch_size=512 \
   loader.batch_size=32 \
   loader.eval_batch_size=32 \
@@ -24,13 +24,13 @@ python -m src.discrete_diffusion \
   callbacks.checkpoint_every_n_steps.every_n_train_steps=2000 \
   trainer.precision=bf16 \
   training.torch_compile=false \
-  model.length=512 \
+  model.length=128 \
   model.dropout=0.0 \
   optim.lr=5e-4 \
   optim.weight_decay=0.02 \
   optim.beta1=0.9 \
   optim.beta2=0.99 \
   optim.eps=1e-9 \
-  lr_scheduler.warmup_t=10000 \
+  lr_scheduler.warmup_t=100 \
   wandb.name='small-gidd-owt-512' \
   wandb.project=UNI-D2
